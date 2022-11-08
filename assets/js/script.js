ef07a338d3935b8011264fb7340862d51eec48f8
sovetbekov <sovvetbekov@gmail.com>
2022-11-08 12:35:36 +0600

    hover effect on socials

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,62 @@
-function myFunction() {
+function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function hoverTelegram(element) {
+    element.setAttribute('src', 'assets/images/socials/hovertelegram.png');
+}
+  
+function unhoverTelegram(element) {
+   element.setAttribute('src', 'assets/images/socials/telegram.png');  
+}
+
+
+
+
+function hoverSpotify(element) {
+    element.setAttribute('src', 'assets/images/socials/hoverspotify.png');
+}
+  
+function unhoverSpotify(element) {
+   element.setAttribute('src', 'assets/images/socials/spotify.png');  
+}
+
+
+
+
+function hoverLinkedin(element) {
+    element.setAttribute('src', 'assets/images/socials/hoverlinkedin.png');
+}
+  
+function unhoverLinkedin(element) {
+   element.setAttribute('src', 'assets/images/socials/linkedin.png');  
+}
+
+
+function hoverGithub(element) {
+    element.setAttribute('src', 'assets/images/socials/hovergithub.png');
+}
+  
+function unhoverGithub(element) {
+   element.setAttribute('src', 'assets/images/socials/github.png');  
+}
+
+
+
+
+function hoverMail(element) {
+    element.setAttribute('src', 'assets/images/socials/hovermail.png');
+}
+  
+function unhoverMail(element) {
+   element.setAttribute('src', 'assets/images/socials/mail.png');  
+}
+
+
+
+
+
+
+
+
